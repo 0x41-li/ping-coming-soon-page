2ba802fbf0da22186d00aa57e96fc301d4618991
Ali Sbane <ali.sbane.dev@gmail.com>
2021-10-12 10:28:43 +0100

    Add error for empty email field

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let form = document.getElementsByTagName("form")[0];
 let email = document.getElementById("email");
+let formParaError = document.getElementById("form-para-error");
 let regExEmail =
   /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -7,9 +8,16 @@ form.addEventListener("submit", formSubmitted);
 
 function formSubmitted(e) {
   e.preventDefault();
-  if (regExEmail.test(email.value)) {
-    form.classList.remove("form-error");
+  if (email.value === "") {
+    formParaError.textContent =
+      "Whoops! It looks like you forgot to add your email";
+      form.classList.add("form-error");
   } else {
-    form.classList.add("form-error");
+    if (regExEmail.test(email.value)) {
+      form.classList.remove("form-error");
+    } else {
+      formParaError.textContent = "Please provide a valid email address";
+      form.classList.add("form-error");
+    }
   }
 }
